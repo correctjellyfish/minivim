126ab7e7f3b4b8ff952e81b685d04b8857d3361b
Braden Griebel <bgriebel@uw.edu>
2025-11-27 12:44:00 -0800

    Added python debug configuration

--- plugin/60_debug.lua
+++ plugin/60_debug.lua
@@ -10,6 +10,8 @@ later(function()
 	Config.nmap_leader("db", require("dap").toggle_breakpoint, "Toggle [B]reakpoint")
 	-- New/Continue session
 	Config.nmap_leader("ds", require("dap").continue, "[S]tart/Continue")
+	-- Disconnect
+	Config.nmap_leader("dq", "<cmd>DapDisconnect<cr>", "[S]tart/Continue")
 	-- Debug Commands
 	Config.nmap("<Down>", "<cmd>DapStepOver<cr>", "Step Over")
 	Config.nmap("<Right>", "<cmd>DapStepInto<cr>", "Step Over")
@@ -30,6 +32,32 @@ later(function()
 		command = "rust-gdb",
 		args = { "--interpreter=dap", "--eval-command", "set print pretty on" },
 	}
+	-- Debugpy (Python)
+	dap.adapters.python = function(cb, config)
+		if config.request == "attach" then
+			---@diagnostic disable-next-line: undefined-field
+			local port = (config.connect or config).port
+			---@diagnostic disable-next-line: undefined-field
+			local host = (config.connect or config).host or "127.0.0.1"
+			cb({
+				type = "server",
+				port = assert(port, "`connect.port` is required for a python `attach` configuration"),
+				host = host,
+				options = {
+					source_filetype = "python",
+				},
+			})
+		else
+			cb({
+				type = "executable",
+				command = "$HOME/.local/bin/debugpy", -- Installed with uv
+				args = { "-m", "debugpy.adapter" },
+				options = {
+					source_filetype = "python",
+				},
+			})
+		end
+	end
 
 	-- Configuration for C/C++
 	dap.configurations.c = {
@@ -42,7 +70,7 @@ later(function()
 			end,
 			args = {}, -- provide arguments if needed
 			cwd = "${workspaceFolder}",
-			stopAtBeginningOfMainSubprogram = false,
+			stopAtBeginningOfMainSubprogram = true,
 		},
 		{
 			name = "Select and attach to process",
@@ -104,6 +132,34 @@ later(function()
 				return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
 			end,
 			cwd = "${workspaceFolder}",
+		},
+	}
+	-- Python configuration
+	dap.configurations.python = {
+		{
+			-- The first three options are required by nvim-dap
+			type = "python", -- the type here established the link to the adapter definition: `dap.adapters.python`
+			request = "launch",
+			name = "Launch file",
+
+			-- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
+
+			program = "${file}", -- This configuration will launch the current file if used.
+			pythonPath = function()
+				-- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
+				-- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
+				-- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
+				local cwd = vim.fn.getcwd()
+				if vim.fn.executable("$VIRTUAL_ENV/bin/python") == 1 then
+					return "$VIRTUAL_ENV/bin/python"
+				elseif vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
+					return cwd .. "/venv/bin/python"
+				elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
+					return cwd .. "/.venv/bin/python"
+				else
+					return "/usr/bin/python"
+				end
+			end,
 		},
 	}
 end)
