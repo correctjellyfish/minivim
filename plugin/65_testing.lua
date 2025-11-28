42db936ffeb4862df3048d2f8503d12b6e4dee3d
Braden Griebel <bgriebel@uw.edu>
2025-11-28 10:12:52 -0800

    Added testing support

---
+++ plugin/65_testing.lua
@@ -0,0 +1,21 @@
+-- Make concise helpers for installing/adding plugins in two stages
+local add, later = MiniDeps.add, MiniDeps.later
+
+-- Neotest ========================================================
+later(function()
+	add({
+		source = "nvim-neotest/neotest",
+		depends = {
+			"nvim-neotest/nvim-nio",
+			"nvim-lua/plenary.nvim",
+			"antoinemadec/FixCursorHold.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"orjangj/neotest-ctest", -- C++ test runner (ctest)
+			"nvim-neotest/neotest-python", -- Python test runner
+		},
+	})
+
+	require("neotest").setup({
+		adapters = require("neotest-python"),
+	})
+end)
