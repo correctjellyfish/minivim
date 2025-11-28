824c45ae8de1c71a0fa434bddf2d4b57e66b2890
Braden Griebel <bgriebel@uw.edu>
2025-11-28 13:47:27 -0800

    Added keymaps for switching to header/source file

---
+++ after/ftplugin/cpp.lua
@@ -0,0 +1,2 @@
+vim.keymap.set("n", "<leader>lh", "<cmd>LspClangdSwitchSourceHeader<cr>", { desc = "Switch to header/source" })
+vim.keymap.set("n", "<leader>lI", "<cmd>LspClangdShowSymbolInfo<cr>", { desc = "Show symbol info" })
