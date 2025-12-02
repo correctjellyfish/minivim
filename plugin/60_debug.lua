173371da259845e68d3f39dc0ffe74858457ad81
Braden Griebel <bgriebel@uw.edu>
2025-12-02 13:59:47 -0800

    Updating testing keymaps, and python debug config

--- plugin/60_debug.lua
+++ plugin/60_debug.lua
@@ -41,33 +41,6 @@ later(function()
 		name = "lldb",
 	}
 
-	-- Debugpy (Python)
-	dap.adapters.python = function(cb, config)
-		if config.request == "attach" then
-			---@diagnostic disable-next-line: undefined-field
-			local port = (config.connect or config).port
-			---@diagnostic disable-next-line: undefined-field
-			local host = (config.connect or config).host or "127.0.0.1"
-			cb({
-				type = "server",
-				port = assert(port, "`connect.port` is required for a python `attach` configuration"),
-				host = host,
-				options = {
-					source_filetype = "python",
-				},
-			})
-		else
-			cb({
-				type = "executable",
-				command = "$HOME/.local/bin/debugpy", -- Installed with uv
-				args = { "-m", "debugpy.adapter" },
-				options = {
-					source_filetype = "python",
-				},
-			})
-		end
-	end
-
 	-- Configuration for C/C++
 	dap.configurations.c = {
 		{
@@ -143,39 +116,12 @@ later(function()
 			cwd = "${workspaceFolder}",
 		},
 	}
-	-- Python configuration
-	dap.configurations.python = {
-		{
-			-- The first three options are required by nvim-dap
-			type = "python", -- the type here established the link to the adapter definition: `dap.adapters.python`
-			request = "launch",
-			name = "Launch file",
+end)
 
-			-- Options below are for debugpy, see https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for supported options
-
-			program = "${file}", -- This configuration will launch the current file if used.
-			pythonPath = function()
-				-- debugpy supports launching an application with a different interpreter then the one used to launch debugpy itself.
-				-- The code below looks for a `venv` or `.venv` folder in the current directly and uses the python within.
-				-- You could adapt this - to for example use the `VIRTUAL_ENV` environment variable.
-				local cwd = vim.fn.getcwd()
-				if vim.fn.executable("$VIRTUAL_ENV/bin/python") == 1 then
-					return "$VIRTUAL_ENV/bin/python"
-				elseif vim.fn.executable(cwd .. "/venv/bin/python") == 1 then
-					return cwd .. "/venv/bin/python"
-				elseif vim.fn.executable(cwd .. "/.venv/bin/python") == 1 then
-					return cwd .. "/.venv/bin/python"
-				else
-					local python_exe = vim.fn.systemlist("which python")[1]
-					if vim.v.shell_error ~= 0 then
-						return "/usr/bin/python"
-					else
-						return python_exe
-					end
-				end
-			end,
-		},
-	}
+-- Nvim-DAP-Python ======================
+later(function()
+	add({ source = "mfussenegger/nvim-dap-python", depends = { "mfussenegger/nvim-dap" } })
+	require("dap-python").setup("uv")
 end)
 
 -- DAP View =============================
@@ -194,7 +140,7 @@ end)
 -- DAP Virtual Text =======================
 later(function()
 	add({ source = "theHamsta/nvim-dap-virtual-text", depends = { "mfussenegger/nvim-dap" } })
-	require("nvim-dap-virtual-text").setup()
+	require("nvim-dap-virtual-text").setup({})
 
 	-- Toggle Virtual Text
 	Config.nmap_leader("dt", "<cmd>DapVirtualTextToggle<cr>", "[T]oggle Virtual Text")
--- plugin/65_testing.lua
+++ plugin/65_testing.lua
@@ -17,9 +17,18 @@ later(function()
 
 	require("neotest").setup({
 		adapters = {
-			require("neotest-python"), -- Python
+			require("neotest-python")({ runner = "pytest" }), -- Python
 			require("rustaceanvim.neotest"), -- Rust
 			require("neotest-ctest"), -- C++
 		},
 	})
+
+	local neotest = require("neotest")
+	Config.nmap_leader("nn", neotest.run.run, "Test Nearest")
+	Config.nmap_leader("ns", neotest.run.stop, "Stop Nearest Test")
+	Config.nmap_leader("na", neotest.run.attach, "Attach Nearest Test")
+	Config.nmap_leader("nd", ":lua require('neotest').run.run({strategy='dap'})<cr>", "Debug Nearest Test")
+	Config.nmap_leader("nf", ":lua require('neotest').run.run(vim.fn.expand('%'))<cr>", "Test File")
+	Config.nmap_leader("nw", ":lua require('neotest').watch.watch(vim.fn.expand('%'))<cr>", "Watch File")
+	Config.nmap_leader("no", ":lua require('neotest').output_panel.toggle()<cr>", "Toggle Test Ouput")
 end)
