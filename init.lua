d74120bdfd50960dd15cc2c462744dcf8f06b44f
Braden Griebel <bgriebel@uw.edu>
2025-12-11 17:31:24 -0800

    Removed redundant function

--- init.lua
+++ init.lua
@@ -33,28 +33,3 @@ end
 
 -- Create a function to delay plugin loading if no args provided to nvim
 _G.Config.now_if_args = vim.fn.argc(-1) > 0 and MiniDeps.now or MiniDeps.later
-
--- Create a function for finding the git root of a project
--- Based on function from NixCats example config
-function _G.find_git_root()
-	-- Use the current buffer's path as the starting point for the git search
-	local current_file = vim.api.nvim_buf_get_name(0)
-	local current_dir
-	local cwd = vim.fn.getcwd()
-	-- If the buffer is not associated with a file, return nil
-	-- Or if the buffer is the ministarter (as that isn't associated with the correct filepath)
-	if current_file == "" or string.find(current_file, "^ministarter:") then
-		current_dir = cwd
-	else
-		-- Extract the directory from the current file's path
-		current_dir = vim.fn.fnamemodify(current_file, ":h")
-	end
-
-	-- Find the Git root directory from the current file's path
-	local git_root = vim.fn.systemlist("git -C " .. vim.fn.escape(current_dir, " ") .. " rev-parse --show-toplevel")[1]
-	if vim.v.shell_error ~= 0 then
-		print("Not a git repository. Searching on current working directory")
-		return cwd
-	end
-	return git_root
-end
