d23e1d7480682b9f1e638472a95b2f24b0a8a325
Braden Griebel <bgriebel@uw.edu>
2026-01-08 11:18:38 -0800

    Changed from mypy to ty for typechecking python

--- plugin/50_languages.lua
+++ plugin/50_languages.lua
@@ -185,7 +185,7 @@ later(function()
 	require("lint").linters_by_ft = {
 		markdown = { "markdownlint-cli2" },
 		bash = { "shellcheck" },
-		python = { "mypy" },
+		python = { "ty" },
 		javascript = { "oxlint", "eslint" },
 		typscript = { "eslint" },
 	}
