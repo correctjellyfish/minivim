e1f9650d8def7229eb23cacabedf97cad50259f9
Braden Griebel <bgriebel@uw.edu>
2026-01-26 20:23:52 -0800

    Removed mypy in favor of ty

--- plugin/50_languages.lua
+++ plugin/50_languages.lua
@@ -185,7 +185,6 @@ later(function()
 	require("lint").linters_by_ft = {
 		markdown = { "markdownlint-cli2" },
 		bash = { "shellcheck" },
-		python = { "mypy" },
 		javascript = { "oxlint", "eslint" },
 		typscript = { "eslint" },
 	}
