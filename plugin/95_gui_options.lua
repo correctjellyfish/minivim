98b57e1bf1314723138e09ba47549bced39e9362
Braden Griebel <bgriebel@uw.edu>
2026-04-17 15:57:51 -0700

    Disable mini animate in vscode plugin

--- plugin/95_gui_options.lua
+++ plugin/95_gui_options.lua
@@ -1,3 +1,7 @@
 vim.o.guifont = "FiraCode Nerd Font Mono:h12"
 vim.g.neovide_hide_mouse_when_typing = true
 vim.g.neovide_remember_window_size = true
+
+if vim.g.vscode then
+	vim.g.minianimate_disable = true
+end
