a4419816281764da58b9e7bfa5636a7d9ada646d
Braden Griebel <bgriebel@uw.edu>
2026-04-22 13:41:13 -0700

    Added signature and completion to blink.cmp

--- plugin/50_languages.lua
+++ plugin/50_languages.lua
@@ -11,11 +11,12 @@ now_if_args(function()
 	require("blink.cmp").setup({
 		keymap = { preset = "enter" },
 		appearance = { nerd_font_variant = "mono" },
-		completion = { documentation = { auto_show = false } },
+		completion = { documentation = { auto_show = true } },
 		sources = {
 			default = { "lsp", "path", "snippets", "buffer" },
 		},
 		fuzzy = { implementation = "prefer_rust_with_warning" },
+		signature = { enabled = true },
 	})
 end)
 
