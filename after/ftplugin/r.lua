f5ae9ca9241b185c92a92e6d2a2e3f272fe691a3
Braden Griebel <bgriebel@uw.edu>
2026-04-22 10:44:34 -0700

    Added method to get semicolon easier in R

--- after/ftplugin/r.lua
+++ after/ftplugin/r.lua
@@ -1,2 +1,3 @@
 vim.keymap.set("i", "<M-->", "<-", { desc = "Insert arrow" })
 vim.keymap.set("i", "<localleader>,", "|>", { desc = "Insert pipe" })
+vim.keymap.set("i", "<localleader>;", ";", { desc = "Insert pipe" })
