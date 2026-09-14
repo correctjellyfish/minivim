05b87843839dff84cef4c811c53bd7e7b51e1d84
Braden Griebel <bgriebel@uw.edu>
2026-09-14 09:40:08 -0700

    Add friendly-snippets, move snippets to languages file

--- plugin/50_languages.lua
+++ plugin/50_languages.lua
@@ -34,6 +34,34 @@ now_if_args(function()
 	vim.lsp.config("*", { capabilities = MiniCompletion.get_lsp_capabilities() })
 end)
 
+-- Snippets
+later(function()
+	-- Add Friendly snippets
+	add({ "https://github.com/rafamadriz/friendly-snippets.git" })
+	-- Define language patterns to work better with 'friendly-snippets'
+	local latex_patterns = { "latex/**/*.json", "**/latex.json" }
+	local lang_patterns = {
+		tex = latex_patterns,
+		plaintex = latex_patterns,
+		-- Recognize special injected language of markdown tree-sitter parser
+		markdown_inline = { "markdown.json" },
+	}
+
+	local snippets = require("mini.snippets")
+	local config_path = vim.fn.stdpath("config")
+	snippets.setup({
+		snippets = {
+			-- Always load 'snippets/global.json' from config directory
+			snippets.gen_loader.from_file(config_path .. "/snippets/global.json"),
+			-- Load from 'snippets/' directory of plugins, like 'friendly-snippets'
+			snippets.gen_loader.from_lang({ lang_patterns = lang_patterns }),
+		},
+	})
+
+	-- Start LSP server to integrate with mini.completion
+	MiniSnippets.start_lsp_server()
+end)
+
 -- Tree-sitter ================================================================
 now_if_args(function()
 	-- Define hook to update tree-sitter parsers after plugin is updated
