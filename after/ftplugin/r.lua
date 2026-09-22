243b4b6256c40084f57d5c9c083751e7c933dadc
Braden Griebel <bgriebel@uw.edu>
2026-09-21 20:52:02 -0700

    Change R insert pipe mapping

--- after/ftplugin/r.lua
+++ after/ftplugin/r.lua
@@ -1,3 +1,2 @@
 vim.keymap.set("i", "<M-->", "<-", { desc = "Insert arrow" })
-vim.keymap.set("i", "<localleader>,", "|>", { desc = "Insert pipe" })
-vim.keymap.set("i", "<localleader>;", ";", { desc = "Insert pipe" })
+vim.keymap.set("i", "<C-,>", "|>", { desc = "Insert pipe" })
